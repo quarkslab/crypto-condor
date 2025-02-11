a8f7a63986aad89080ac0af1569d255b487d0891
JulioLoayzaM <jloayzameneses@quarkslab.com>
2025-02-11 01:35:23 +0100

    Update the AES module
    Add the enums from the former vectors.AES module.
    
    Update the debut data classes to simplify usage.
    
    Update how the test vectors are loaded, using the JSON file and the new
    AesVectors class.
    
    Remove the test() function in favour of test_encrypt() and
    test_decrypt(). The motivation is twofold: first, as with recent primitives,
    offer one function to test one operation instead of trying to have one
    function that dispatches tests. This avoids adding too many arguments
    and potentially unintuitive None parameters when calling the function.
    Second, the internal functions (_test_*_encrypt, _test_*_decrypt) had to
    be updated to make use of the new test vectors. Since these vectors do
    not differentiate between sources or modes, they can be handled more
    easily by a single auxiliary function (_try_one_enc and _try_one_dec).
    
    Remove verify_file(), replacing it with test_output_encrypt() and
    test_output_decrypt(). Same logic as above, offer one function to test
    one operation. It also improves the parsing of the line, matching for
    expected patterns and checking whether they correspond to the given
    mode.
    
    Mark the handling of invalid tests with FIXME tags. Currently, we only
    check if the implementation "failed" when using invalid tests, for a
    very large definition of "failed". Ideally, we should use (and maybe
    add) the flags to check for specific errors. This could be accomplished
    in the Python wrappers with custom Exceptions.
    
    Update the Python runner to use a new naming convention, similar to the
    harness API, making use of the new test_encrypt() and test_decrypt()
    functions.
    
    Update the internal harness function to call the correct tests.

--- tests/primitives/test_AES.py
+++ tests/primitives/test_AES.py
@@ -9,26 +9,27 @@ console = Console()
 
 
 @pytest.mark.parametrize("mode", AES.Mode)
-def test_mode(mode: AES.Mode):
-    """Tests the correctness of the internal implementation."""
+def test_encrypt(mode: AES.Mode):
+    """Tests internal encryption."""
 
-    # Define inner functions to set the correct mode.
     def _encrypt(key, pt, *, iv=None, aad=None, mac_len=0):
         return AES._encrypt(mode, key, pt, iv=iv, aad=aad, mac_len=mac_len)
+
+    rd = AES.test_encrypt(_encrypt, mode, AES.KeyLength.ALL, resilience=True)
+    console.print_results(rd)
+    assert rd.check()
+
+
+@pytest.mark.parametrize("mode", AES.Mode)
+def test_decrypt(mode: AES.Mode):
+    """Tests internal decryption."""
 
     def _decrypt(
         key, ct, *, iv=None, segment_size=0, aad=None, mac=None, mac_len=0, tag=None
     ):
-        return AES._decrypt(
-            mode,
-            key,
-            ct,
-            iv=iv,
-            aad=aad,
-            mac=mac,
-            mac_len=mac_len,
-        )
+        return AES._decrypt(mode, key, ct, iv=iv, aad=aad, mac=mac, mac_len=mac_len)
 
-    results_dict = AES.test(_encrypt, _decrypt, mode, AES.KeyLength.ALL)
-    console.print_results(results_dict)
-    assert results_dict.check()
+    rd = AES.test_decrypt(_decrypt, mode, AES.KeyLength.ALL, resilience=True)
+    print(rd.keys())
+    console.print_results(rd)
+    assert rd.check()
