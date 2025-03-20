9217bce4e5df863c97e9ce0a85de16beca84b9bc
JulioLoayzaM <jloayzameneses@quarkslab.com>
2025-03-20 10:32:10 +0100

    Update the SHA wrapper template
    Uses the new wrapper API, with the same examples as the actual example
    wrapper.

--- crypto_condor/resources/wrappers/SHA/Python/sha_wrapper.py
+++ crypto_condor/resources/wrappers/SHA/Python/sha_wrapper.py
@@ -1,8 +1,15 @@
-"""Wrapper template for SHA implementations."""
+"""Wrapper template for SHA implementations.
+
+Full API documentation at:
+https://quarkslab.github.io/crypto-condor/latest/wrapper-api/SHA.html
+
+Usage:
+    crypto-condor-cli test wrapper SHA sha_wrapper.py
+"""
 
 
-def sha(data: bytes) -> bytes:
-    """Wrapper function for a SHA implementation.
+def CC_SHA_256_digest(data: bytes) -> bytes:
+    """Wrapper function for a SHA-256 implementation.
 
     Args:
         data: The input data.
@@ -10,4 +17,27 @@ def sha(data: bytes) -> bytes:
     Returns:
         The digest of the data.
     """
-    pass
+    raise NotImplementedError
+
+def CC_SHA_3_384_digest(data: bytes) -> bytes:
+    """Wrapper function for a SHA3-384 implementation.
+
+    Args:
+        data: The input data.
+
+    Returns:
+        The digest of the data.
+    """
+    raise NotImplementedError
+
+
+def CC_SHA_512_224_digest(data: bytes) -> bytes:
+    """Wrapper function for a SHA-512/224 implementation.
+
+    Args:
+        data: The input data.
+
+    Returns:
+        The digest of the data.
+    """
+    raise NotImplementedError
