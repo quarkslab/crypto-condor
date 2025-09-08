9c4b4738ae90a316c7fa1bc6d5e6e1c0e2f40ea3
JulioLoayzaM <jloayzameneses@quarkslab.com>
2025-09-08 14:25:03 +0200

    Fix typer conflict with click 8.2.1
    Typer 0.9.4 is not compatible with click 8.2.1 due to a missing
    positional argument in rich_utils.py:370, so update Typer to the latest
    version.
    
    This introduced a breaking change, as the rich_utils module imported to
    override the dim text in help messages is now lazy-loaded [1]. The
    solution is to directly import rich_utils from typer.
    
    Typer was not updated due to using the caret requirement [2]. The
    constraint stays for now, considering potential breaking changes
    introduced in new versions. However, the constraint on lief changes
    ~=0.16, a compatible release [3] allowing >=0.16.0 and <1.0.0.
    
    [1]: https://github.com/fastapi/typer/pull/1128
    [2]: https://python-poetry.org/docs/dependency-specification/#caret-requirements

--- crypto_condor/cli/__init__.py
+++ crypto_condor/cli/__init__.py
@@ -1,8 +1,11 @@
 """CLI module for crypto-condor."""
 
-import typer
+from typer import rich_utils
 
 # Hacky fix for dull help text on some terminals.
 # https://github.com/tiangolo/typer/issues/437
-typer.rich_utils.STYLE_HELPTEXT_FIRST_LINE = "bold"
-typer.rich_utils.STYLE_HELPTEXT = ""
+# Update 08/09/2025: rich_utils is now lazy-loaded (see
+# https://github.com/fastapi/typer/pull/1128) so we have to explicitly import it from
+# typer. Override works the same way as before.
+rich_utils.STYLE_HELPTEXT_FIRST_LINE = "bold"
+rich_utils.STYLE_HELPTEXT = ""
